ad93031839a6a07276befb20670549e86d775931
Matheus Helinski <100943965+helinskimatheus@users.noreply.github.com>
2022-04-28 01:16:43 -0300

    New updated files.
    O projeto agora contém novos arquivos mais atualizados!

---
+++ Teste Processo seletido MTD/Arquivos Do Desafio/JavaScript/mobile.js
@@ -0,0 +1,43 @@
+
+function showMenuMobile(){
+    let navBarMobile = document.querySelector('.nav-bar-mobile');
+    if(navBarMobile.classList.contains('open')){
+        navBarMobile.classList.remove('open');
+        document.querySelector('.icon').src = "./Assets/Icons/icon-menu.svg";
+    }
+    else{
+        navBarMobile.classList.add('open');
+        document.querySelector('.icon').src = "./Assets/Icons/icon-close-menu.svg";
+    }
+}
+function showBackgroundMenuMobile(){
+    let showBackground = document.querySelector('.backgroundMenuMobile')
+    if(showBackground.classList.contains('addBackGroundMenuMobile')){
+        showBackground.classList.remove('addBackGroundMenuMobile')
+    }
+    else{
+        showBackground.classList.add('addBackGroundMenuMobile')
+    }
+}
+function showMenuFeaturesMobile(){
+    let showDropdown = document.querySelector('.features-menu-mobile');
+    if(showDropdown.classList.contains('active-features-mobile')){
+        document.querySelector('.icon-features-mobile').src = "./Assets/Icons/icon-arrow-down.svg";
+        showDropdown.classList.remove('active-features-mobile');
+    }
+    else{
+        document.querySelector('.icon-features-mobile').src = "./Assets/Icons/icon-arrow-up.svg";
+        showDropdown.classList.add('active-features-mobile');
+    }
+}
+function showMenuCompanyMobile(){
+    let showDropdown = document.querySelector('.company-menu-mobile');
+    if(showDropdown.classList.contains('active-company-mobile')){
+        document.querySelector('.icon-company-mobile').src = "./Assets/Icons/icon-arrow-down.svg";
+        showDropdown.classList.remove('active-company-mobile');
+    }
+    else{
+        document.querySelector('.icon-company-mobile').src = "./Assets/Icons/icon-arrow-up.svg";
+        showDropdown.classList.add('active-company-mobile');
+    }
+}
